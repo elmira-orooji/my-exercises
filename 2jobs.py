d662457635732bf468a760dace7d6dd225aabc2d
elmira-orooji <elmiraorooji78@gmail.com>
2026-02-17 16:06:00 +0330

    2jobs

---
+++ 2jobs.py
@@ -0,0 +1,8 @@
+user_input = input()
+
+if user_input == "shanbe" or user_input == "doshanbe" or user_input == "chaharshanbe":
+    print("perspolis")
+elif user_input == "jome":
+    print("tatil")
+else:
+    print("bahman")
