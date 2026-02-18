5ac13d0332fbcde737a0cc141cde4a90a323448a
elmira-orooji <elmiraorooji78@gmail.com>
2026-02-19 00:13:40 +0330

    m-sin

---
+++ m-sin.py
@@ -0,0 +1,11 @@
+m = int(input())
+
+counter = 0 
+
+sin_list = ['sib', 'senjed', 'samanoo', 'sir', 'somagh', 'sekeh', 'serkeh']
+
+for i in sin_list:
+    print(i)
+    counter += 1
+    if counter == m:
+        break
