0ac55e2b7cfe0680eeee36301f5ea9f0eeef424c
elmira-orooji <elmiraorooji78@gmail.com>
2026-05-21 23:27:42 +0330

    gui-bmi-calculator Completed

--- gui-bmi-calculator.py
+++ gui-bmi-calculator.py
@@ -5,36 +5,50 @@ from PyQt6.QtCore import QSize, Qt
 app = QApplication(sys.argv)
 
 # logic
+def calculate_bmi_and_result():
+    result = None
+
+    weight = float(weight_entry.text())
+    height = float(height_entry.text())
+
+    bmi = weight // (height * 2)
+
+    if( 18.5 <= bmi < 25):
+        result = "Normal"
+    
+    elif( 25 <=  bmi < 30):
+       result = "Over weight"
+    
+    elif( 30 <= bmi < 35):
+       result  = "Obese"
+    
+    elif(bmi < 18.5):
+        result = "Underweight"
+
+    else:
+        result = "Extremely obese"
+
+    result_label.setText(f"Result : {result}")
 
 # ui design 
-
 window = QMainWindow()
 window.setWindowTitle("GUI BMI CALCULATOR")
 window.setFixedSize(QSize(400, 300))
 widget = QWidget()
 layot = QVBoxLayout()
-
-
 height_label = QLabel("Height (m):")
 height_entry = QLineEdit()
-
 weight_label = QLabel("Weight (kg):")
 weight_entry = QLineEdit()
-
-result_label = QLabel("Result:")
-
 calculate_button = QPushButton(text="Calculate BMI")
-
-
+calculate_button.clicked.connect(calculate_bmi_and_result)
+result_label = QLabel("Result:")
 layot.addWidget(height_label)
 layot.addWidget(height_entry)
-
 layot.addWidget(weight_label)
 layot.addWidget(weight_entry)
-
-layot.addWidget(result_label)
-
 layot.addWidget(calculate_button)
+layot.addWidget(result_label)
 
 widget.setLayout(layot)
 window.setCentralWidget(widget)
